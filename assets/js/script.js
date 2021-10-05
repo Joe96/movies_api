c95b7ab5c3bea52ad0401ac87e46a3898115f5dd
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-10-05 10:28:54 -0500

    second code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,41 @@
 const apiKey = "64dc8221474533792ea48b35c977b47c";
-function getPopMovies(genre){
- 
-//fetching url 
+var genre = ""
+
+//Fetching popular movies from Movieurl
+function getPopMovies(){
     var requestUrl = `https://api.themoviedb.org/3/discover/movie?&api_key=64dc8221474533792ea48b35c977b47c&with_genres=${genre}`
     fetch(requestUrl)
     .then(function(response){
-        response.json().then(function (data) {
-          console.log(data);
-          if(data != ""){
-            localStorage.clear();
-
-            localStorage.setItem("movieList",JSON.stringify(data));
-  
-            window.location="./index1.html";
-          }else{
-             alert("Please ")
-          }
-         
-        
-        });
+         response.json().then(function (data) {
+         //Storing data in localstorage
+         localStorage.setItem("movielist", JSON.stringify(data.results.slice(0,3)));
+         window.location="./index1.html";
+        })
+       
     })
+}
 
-    
-  }
-
-  $("#favoriteMovies").change(function(){
+function getAllGenre() {
+    var requestUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=64dc8221474533792ea48b35c977b47c&language=en-US`
+    fetch(requestUrl)
+    .then(function(response){
+         response.json().then(function (data) {
+         //Storing data in localstorage
+          console.log(data)
+          var selectGenre = document.getElementById("favoriteMovies");
+          selectGenre.innerHTML =  `<option value=""> </option>`  
+            for(var i=0; i<data.genres.length;i++) {
+               selectGenre.innerHTML = selectGenre.innerHTML+  `<option value="${data.genres[i].id}">${ data.genres[i].name} </option>`  
+            }
+        })
+       
+    })
+}
+getAllGenre();
+//Function for selecting dropdown movies type
+$("#favoriteMovies").change(function() {
     //alert($("#favoriteMovies").val());
-      getPopMovies($("#favoriteMovies").val());
-  });
-
+    genre = $("#favoriteMovies").val();
+    console.log("Genre chosen:", genre);
+    getPopMovies();
+})
