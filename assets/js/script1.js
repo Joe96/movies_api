ebd72156ab5f481a506d8776d69e7e5690932fe6
tcrear <smileybear88@hotmail.com>
2021-10-05 20:11:24 -0500

    Updating local changes to group repo.

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,7 +1,9 @@
 //Getting movielist from local storage
 var movieDetails = JSON.parse(localStorage.getItem("movielist"));
 console.log(movieDetails);
+
 populateMovies(movieDetails); 
+
 //Populate movies from movielist
 function populateMovies(movieDetails) {
   for(var i=0;i<3;i++){
@@ -9,11 +11,13 @@ function populateMovies(movieDetails) {
     <div class="col s12 l3 center-align" id="Movie0" sytle="width:33%">
     <!-- Div to display content of the Movie.-->
     <div class="col s12 l8 left-align center" id="infoColumn">
-      <p  id="title" style="text-align: left;">Title :${movieDetails[i].original_title} </p>
-      <p  id="description" style="text-align: left;"> About :${movieDetails[i].overview} </p><br>
-      <p  id="popularity" style="text-align: left;"> Rating :${movieDetails[i].popularity} </p><br>
-      <p  id="releaseDate" style="text-align: left;"> Released On :${movieDetails[i].release_date} </p><br>
-      <img src ="https://image.tmdb.org/t/p/w300/${movieDetails[i].poster_path}"  class="responsive-img" id="movie0" alt="Coming Soon...!!!"/>
+    <img src ="https://image.tmdb.org/t/p/w300/${movieDetails[i].poster_path}"  class="responsive-img" id="movie0" alt="Coming Soon...!!!"/>
+     /<div> 
+     <div>
+      <p  id="title" style="text-align: left;">Title: ${movieDetails[i].original_title} </p>
+      <p  id="description" style="text-align: left;"> About: ${movieDetails[i].overview} </p><br>
+      <p  id="popularity" style="text-align: left;"> Rating: ${movieDetails[i].vote_average} </p><br>
+      <p  id="releaseDate" style="text-align: left;"> Released On: ${movieDetails[i].release_date} </p><br>
     </div>
    </div> `)
   }
